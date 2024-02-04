5bba3f135484c8f0b5659a6b5c6083946d32cdcf
avrahamavitan <155629131+avrahamavitan@users.noreply.github.com>
2024-02-05 00:40:36 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -16,8 +16,8 @@ int j;
 scanf(" %d",&i);
 scanf(" %d",&j);
 int x=arr[i][j];
-if(x!=0){printf("true\n");}
-else printf("false\n");
+if(x!=0){printf("true");}
+else printf("false");
  
 }
 
@@ -50,6 +50,6 @@ scanf(" %d",&i);
 scanf(" %d",&j);
 int x=arr1[i][j];
 
-if(x!=0){printf(" %d\n", x);}
-else printf(" %d\n", -1);
+if(x!=0){printf(" %d", x);}
+else printf(" %d", -1);
 }
