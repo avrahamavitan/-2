707c588d038ac258fd0c6758722a484c35458a89
avrahamavitan <155629131+avrahamavitan@users.noreply.github.com>
2024-02-05 14:02:39 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -28,15 +28,15 @@ void fanc3(int arr[10][10]){
     int arr1[10][10];
     for(int i=0;i<10;i++){
         for(int j=0;j<10;j++){
-            if(arr[i][j]!=0){arr1[i][j]=arr[i][j];}
-          else arr1[i][j]=999;
+            arr1[i][j]=arr[i][j];
+          
         }
         
     }
     for(int k=0;k<10;k++){
         for (int i=0;i<10;i++){
              for(int j=0;j<10;j++){
-                if(arr1[i][k]!=999&&arr1[k][j]!=999){
+                if(arr1[i][k]!=0&&arr1[k][j]!=0){
                 if (arr1[i][k]+arr1[k][j]<arr1[i][j]){
                     arr1[i][j]=arr1[i][k]+arr1[k][j];
                 }
@@ -56,6 +56,6 @@ scanf(" %d",&i);
 scanf(" %d",&j);
 int x=arr1[i][j];
 
-if(x!=999){printf("%d\n", x);}
+if(x!=0){printf("%d\n", x);}
 else printf("%d\n", -1);
 }
